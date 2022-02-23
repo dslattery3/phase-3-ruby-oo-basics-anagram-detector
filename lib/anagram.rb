e80ddcb0c5e40ba95c9278e53cbdcd2aaa5a445f
Dalton <slattery.da@gmail.com>
2022-02-23 16:03:55 -0600

    commit tree 6f41478295973d1e49fe25154a3b8eac80b6cfd9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        word_arr = @word.chars
+        new_arr = []
+
+        arr.each do |word|
+            if word.chars.sort == word_arr.sort
+                new_arr << word
+            end
+        end
+        return new_arr
+    end
+
+
+end
